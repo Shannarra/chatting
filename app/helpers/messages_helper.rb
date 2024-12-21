355e5bd56429e199783609d0e97722f2ae7f7419
Petar <petarangelov15@gmail.com>
2024-12-21 16:56:35 +0200

    Added some styling

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,13 @@
 module MessagesHelper
+  def justify_and_color_from(message, owned)
+    if owned
+      %w[justify-end bg-green-600 ]
+    else
+      %w[justify-start bg-blue-600 ]
+    end
+  end
+
+  def margin_by_message(message, same_user)
+    same_user ? "mt-0" : "mt-2"
+  end
 end
